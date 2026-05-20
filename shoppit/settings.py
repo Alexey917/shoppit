974b6ade51ed0d5fc9f80625992fd3143afbd85f
Alexey917 <pochtovii.1996@mail.ru>
2026-05-20 20:17:13 +0300

    добавил media

--- shoppit/settings.py
+++ shoppit/settings.py
@@ -124,5 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'img/'
+MEDIA_ROOT = BASE_DIR/"media"
 
 AUTH_USER_MODEL = "core.CustomerUser"
